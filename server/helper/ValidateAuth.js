a8a96b60f3eb6a4438f9a45284836c060b216f7f
diama1 <danimahoro75@gmail.com>
2019-03-15 12:11:03 +0200

    this commit adds validation and modify test.

--- server/helper/ValidateAuth.js
+++ server/helper/ValidateAuth.js
@@ -6,17 +6,17 @@ const validateAuthantication = {
     signUpValidation(newUser) {
         let userSchema = {
 
-            name: Joi.string().required(),
-            email: Joi.string().email({ minDomainAtoms: 2 }),
-            password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/)
+            name: Joi.string().alphanum().required(),
+            email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+            password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required()
         }
         return Joi.validate(newUser, userSchema);
     },
 
     loginValidation(newUser) {
         const LoginSchema = {
-            email: Joi.string().email(),
-            password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/)
+            email: Joi.string().email().required(),
+            password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required()
         }
 
         return Joi.validate(newUser, LoginSchema)
